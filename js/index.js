2c77f13cfbe10693621175f81aabe16f10f2e9ec
dasommmm <103475183+dasommmm@users.noreply.github.com>
2022-09-23 13:53:32 +0900

    9.23

--- js/index.js
+++ js/index.js
@@ -33,8 +33,26 @@ for(let i=0; i< eventText.length; i++){
 
 
 
+const rect = document.getElementsByClassName('we-believe')[0];
 
+rect.addEventListener('mousedown', (e) =>{
+    const rectLocation = rect.getBoundingClientRect();
+    const startX = e.clientX
+    document.addEventListener('mousemove',dragThings)
+    document.addEventListener('mouseup', ()=>{
+        document.removeEventListener('mousemove', dragThings)
+    })
 
+    function dragThings(event) {
+        const nowX = event.pageX
+        
+        if(rectLocation.x+nowX-startX>0){
+            rect.style.transform = `translateX(0px)`
+        }else{
+            rect.style.transform = `translateX(${rectLocation.x+nowX-startX}px)`
+        }
+    }
+})
 
 
 
